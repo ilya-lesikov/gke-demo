2b345ddd6e277817f3643792465220957cf96018
Ilya Lesikov <ilya@lesikov.com>
2020-01-14 22:23:38 +0300

    WIP

---
+++ terragrunt-reinit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+tgfiles="$(find "$SCRIPT_DIR/terraform/environments" -name terragrunt.hcl \
+  ! -path "*/.terragrunt-cache/*" | tr '\n' ' ')"
+
+for tgfile in $tgfiles; do
+  cd "$(dirname "$tgfile")"
+  terragrunt init -input=false -upgrade=true || true
+done
+
+
+# | \
+#   xargs dirname | xargs -I $(cd 
