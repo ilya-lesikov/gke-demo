5dc007d8eb4ef2ff41a98431e4523ff20a67bd42
Ilya Lesikov <ilya@lesikov.com>
2020-01-17 05:13:53 +0300

    WIP

--- scripts/terragrunt-reinit.sh
+++ scripts/terragrunt-reinit.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
-tgfiles="$(find "$SCRIPT_DIR/terraform/environments" -name terragrunt.hcl \
+tgfiles="$(find "$SCRIPT_DIR/../terraform/environments" -name terragrunt.hcl \
   ! -path "*/.terragrunt-cache/*" | tr '\n' ' ')"
 
 for tgfile in $tgfiles; do
